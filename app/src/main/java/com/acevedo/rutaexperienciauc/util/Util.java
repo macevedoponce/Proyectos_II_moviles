2176929145393158e1581d347d0e0f4ceb607589
garcia <75753325@continental.edu.pe>
2023-05-02 08:54:40 -0500

    modificacion util

--- app/src/main/java/com/acevedo/rutaexperienciauc/util/Util.java
+++ app/src/main/java/com/acevedo/rutaexperienciauc/util/Util.java
@@ -1,14 +1,14 @@
 package com.acevedo.rutaexperienciauc.util;
 
 public class Util {
-<<<<<<< HEAD
+
     public static final String RUTA="http://192.168.1.46";
     public static final String RUTA_SLIDER="http://192.168.1.46:3000/api/slider";
-    public static final String RUTA_SEDE="http://192.168.1.46:3000/api/sedes";
+
 
     public static final String RUTA_SOLICITAR_INFORMACION="";
-=======
+
     public static final String RUTA_SEDE="http://192.168.1.46:3000/api/sedes"; // todas las sedes
     public static final String RUTA_SEDE_RANDOM="http://192.168.1.46:3000/api/sedesRandom"; // solo 4 sedes
->>>>>>> dev
+
 }
