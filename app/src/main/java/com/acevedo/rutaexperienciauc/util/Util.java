dcea66ff7acebf991bbb7e3d19dcaeded9007c94
richardrnmr <72393325@continental.edu.pe>
2023-05-12 12:38:48 -0500

    guardando favoritos con sharedPreference req 15 al 100

--- app/src/main/java/com/acevedo/rutaexperienciauc/util/Util.java
+++ app/src/main/java/com/acevedo/rutaexperienciauc/util/Util.java
@@ -2,7 +2,7 @@ package com.acevedo.rutaexperienciauc.util;
 
 public class Util {
 
-    public static final String RUTA="http://192.168.1.46:3000"; // solo cambia la su IP
+    public static final String RUTA="http://192.168.1.5:3000"; // solo cambia la su IP
 
     public static final String RUTA_SEDE=RUTA+"/api/sedes"; // todas las sedes
     public static final String RUTA_SEDE_RANDOM=RUTA+"/api/sedes/sedesRandom"; // solo 4 sedes
