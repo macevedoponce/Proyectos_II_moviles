17be50e9a1802baf7c1ab8987bd02471c5daefde
garcia <75753325@continental.edu.pe>
2023-05-12 07:49:31 -0500

    merge dev

--- app/src/main/java/com/acevedo/rutaexperienciauc/util/Util.java
+++ app/src/main/java/com/acevedo/rutaexperienciauc/util/Util.java
@@ -2,18 +2,12 @@ package com.acevedo.rutaexperienciauc.util;
 
 public class Util {
 
+    public static final String RUTA="http://192.168.1.46:3000"; // solo cambia la su IP
 
-    public static final String RUTA="http://192.168.1.46";
-    public static final String RUTA_SLIDER="http://192.168.1.46:3000/api/slider";
+    public static final String RUTA_SEDE=RUTA+"/api/sedes"; // todas las sedes
+    public static final String RUTA_SEDE_RANDOM=RUTA+"/api/sedes/sedesRandom"; // solo 4 sedes
+    public static final String RUTA_CARRERAS=RUTA+"/api/carreras";// todas la experiencias de un ciclo
+    public static final String RUTA_EXPERIENCIA=RUTA+"/api/experiencias"; // todas la experiencias de un ciclo
+    public static final String RUTA_SOLICITAR_INFORMACION=RUTA+"/api/solicitar_informacion"; //envio de datos
 
-    public static final String RUTA_SOLICITAR_INFORMACION="http://192.168.100.10:3000/solicitarinfo"; //envio de datos
-    public static final String RUTA_LLAMARNOMBRE_SEDE="http://192.168.100.10:3000/nombressedes";
-    public static final String RUTA_LLAMARNOMBRE_CARRERA="http://192.168.100.10:3000/nombrescarreras";
-
-
-    public static final String RUTA_SEDE="http://192.168.100.10:3000/api/sedes"; // todas las sedes
-    public static final String RUTA_SEDE_RANDOM="http://192.168.100.10:3000/api/sedesRandom"; // solo 4 sedes
-
-    public static final String RUTA_EXPERIENCIA="http://192.168.100.10:3000/api/experiencias";
-    public static final String RUTA_CARRERAS="http://192.168.100.10:3000/api/carreras";// todas la experiencias de un ciclo
 }
