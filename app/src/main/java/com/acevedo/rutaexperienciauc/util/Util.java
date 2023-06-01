6b91547960a9a1d80107023588c486ead6e588f7
Miguel Acevedo <82989924+macevedoponce@users.noreply.github.com>
2023-06-01 18:29:49 -0500

    eliminando libreria slider y desarrollando uno propio, eliminando sedes aleatorias

--- app/src/main/java/com/acevedo/rutaexperienciauc/util/Util.java
+++ app/src/main/java/com/acevedo/rutaexperienciauc/util/Util.java
@@ -5,13 +5,10 @@ public class Util {
     public static final String RUTA="http://192.168.1.46:3000"; // solo cambia la su IP
     public static final String RUTA_CONTENIDO=RUTA+"/api/contenido"; // todas las sedes
     public static final String RUTA_SEDE=RUTA+"/api/sedes"; // todas las sedes
-    public static final String RUTA_SEDE_RANDOM=RUTA+"/api/sedes/sedesRandom"; // solo 4 sedes
     public static final String RUTA_CARRERAS=RUTA+"/api/carreras";// todas la experiencias de un ciclo
     public static final String RUTA_EXPERIENCIA=RUTA+"/api/experiencias"; // todas la experiencias de un ciclo
     public static final String RUTA_SOLICITAR_INFORMACION=RUTA+"/api/solicitar_informacion"; //envio de datos
-
     public static final String RUTA_PREGUNTAS_FRECUENTES=RUTA+"/api/preguntas_frecuentes"; //tolas preguntas frecuentes
-
     public static final String RUTA_CALIFICAR_EXPERIENCIA=RUTA+"/api/calificar_experiencia/"; //calificar experiencia
     public static final String RUTA_BENEFICIO=RUTA+"/api/beneficio/"; //beneficio por carrera
 
